afce973ef92032f6c8bae15a908ed808c2ea8beb
Ghutra <156716842+Ghutra@users.noreply.github.com>
2025-11-09 11:43:56 -0800

    Update server.js

--- server.js
+++ server.js
@@ -83,6 +83,7 @@ app.get('/api/alliya', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+console.log('Alliya backend initialized. Awaiting queries...');
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
